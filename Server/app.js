0ea933093a9ee1a062ccd42b898fc6d336cc06c7
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-21 18:22:57 +0530

    Added Schema and resolver.

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const multer = require("multer");
+const { graphqlHTTP } = require("express-graphql");
+
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
 
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2tfmflc.mongodb.net/${process.env.DB_DATABASE}`;
 
@@ -48,6 +52,14 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+  })
+);
 
 //error handling middleware.
 app.use((error, req, res, next) => {
